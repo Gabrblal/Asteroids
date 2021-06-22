9cf499fe1fff8f4a038d525364afe07061df3d42
Gabriel <gabrielblomberg0@gmail.com>
2021-06-22 18:46:47 +1000

    Fixed main.c     - Removed list test code.     - Uncommented basic sfml window code.     - Used argc and argv to suppress compiler warnings.

--- src/main.c
+++ src/main.c
@@ -2,37 +2,27 @@
 
 #include <SFML/Window.h>
 
-#include "list.h"
-
-int main(int argc , char ** argv)
+int main(int argc, char ** argv)
 {
-    // sfWindow *window = sfWindow_create(
-    //     (sfVideoMode){500, 500, 255},
-    //     "Astroids",
-    //     sfDefaultStyle,
-    //     NULL
-    // );
+    if (argc);
+    if (argv);
 
-    // while (sfWindow_isOpen(window))
-    // {
-    //     sfEvent event;
-    //     while (sfWindow_pollEvent(window, &event))
-    //     {
-    //         if (event.type == sfEvtClosed) {
-    //             sfWindow_close(window);
-    //         }
-    //     }
-    // }
+    sfWindow *window = sfWindow_create(
+        (sfVideoMode){500, 500, 255},
+        "Astroids",
+        sfDefaultStyle,
+        NULL
+    );
 
-    List *list = list_create(sizeof(int));
-    for (int i = 0; i < 10; i++) {
-        list_push_front(list, &i);
-    }
-
-    for (int i = 0; i < 10; i++) {
-        int j;
-        list_pop_back(list, &j); 
-        printf("%i", j);
+    while (sfWindow_isOpen(window))
+    {
+        sfEvent event;
+        while (sfWindow_pollEvent(window, &event))
+        {
+            if (event.type == sfEvtClosed) {
+                sfWindow_close(window);
+            }
+        }
     }
 
     return 0;
