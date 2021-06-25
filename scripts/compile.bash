ade51b813a6e2bceb909b9c96dccfca640172a68
Gabriel <gabrielblomberg0@gmail.com>
2021-06-25 15:09:06 +1000

    Added debug flag to gcc.

--- scripts/compile.bash
+++ scripts/compile.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -o bin/Asteroids -Isrc $(find src -name '*.c') $(sdl2-config --cflags --libs)
+gcc -g -o bin/Asteroids -Isrc $(find src -name '*.c') $(sdl2-config --cflags --libs)
--- scripts/compile.bat
+++ scripts/compile.bat
@@ -6,7 +6,7 @@ findstr /R .*\.c$ > ../source.txt && cd ..
 @REM This also means we can't pipe files from findstr into gcc.
 powershell -c "(gc source.txt) -replace '\\', '\\' | Out-File -encoding ASCII 'source.txt'"
 
-gcc @source.txt -o bin/Asteroids.exe -static -static-libgcc ^
+gcc @source.txt -g -o bin/Asteroids.exe -static -static-libgcc ^
 -Ilib/SDL/include -Llib/SDL/lib ^
 -lmingw32 -lSDL2main -lSDL2 -mwindows -lm -ldinput8 -ldxguid -ldxerr8 ^
 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 ^
