25d5d315b670d33a31bc381f661c15836c7506b1
Gabriel <gabrielblomberg0@gmail.com>
2021-06-29 21:02:30 +1000

    Cleaned main, added SDL_Quit().

--- src/main.c
+++ src/main.c
@@ -5,16 +5,22 @@
 
 int main(int argc, char* argv[])
 {
-    // Initialise SDL
+    // Initialise SDL.
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         printf("Failed to initialise SDL: %s. Exiting.", SDL_GetError());
         SDL_Quit();
         exit(1);
     }
 
+    // Create the controller.
     Controller *controller = controller_create();
+
+    // Run the controller event handling thread.
     controller_thread(controller);
+
+    // Destroy the controller.
     controller_destroy(controller);
 
+    SDL_Quit();
     return 0;
 }
