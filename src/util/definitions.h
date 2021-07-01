4966a91ff62b940142a4b83b82de2a51f644f359
Gabriel <gabrielblomberg0@gmail.com>
2021-07-01 19:06:49 +1000

    Renamed elements in Direction enum.

--- src/util/definitions.h
+++ src/util/definitions.h
@@ -5,14 +5,14 @@
  * Generic direction definition in 3D space along the NORTH-EAST, 
  * SOUTH-WEST and IN-OUT axes.
  */
-typedef enum Direction Direction;
-enum Direction {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST,
-    IN,
-    OUT
-};
+typedef enum {
+    DIRECTION_NONE,
+    DIRECTION_NORTH,
+    DIRECTION_EAST,
+    DIRECTION_SOUTH,
+    DIRECTION_WEST,
+    DIRECTION_IN,
+    DIRECTION_OUT
+} Direction;
 
 #endif // DEFINITIONS_H
