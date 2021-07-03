5fd35e7452f50f2e5c21b492b0bdd8ec2d7b9730
Gabriel <gabrielblomberg0@gmail.com>
2021-07-03 10:29:57 +1000

    Reorganised View, made independent of model.     - Moved the View struct definition to the header file again, to       allow drawing functions to be defined outside view.c.     - Removed spin_view_update().     - View now requires a draw_function() to be passed to it on creation       that is called to draw onto the window, and a void* to foward       to the draw_function() for the drawn data. This makes the view       independent from the model.     - Made the timer callbacks public and reorganised view.c for       readability.     - View creation now takes a port callback, so the view is not       responsible for defining the behaviour of the view port.

--- src/view/view.h
+++ src/view/view.h
@@ -5,6 +5,7 @@
 #include "spin/spin.h"
 #include "util/definitions.h"
 #include "util/vector2.h"
+#include "view/view_port.h"
 
 /**
  * Represents the graphical component of the application, keeping track of the
@@ -12,17 +13,57 @@
  * around) and drawing the model to the current view.
  */
 typedef struct View View;
+struct View {
+    // Main game window, used for making the renderer.
+    SDL_Window *window;
+    // Renderer used to clear, draw and render the screen.
+    SDL_Renderer *renderer;
+    // View mutex, to prevent data access during drawing, or concurrent drawing.
+    SDL_mutex *mutex;
+    // Thread drawing to the window.
+    SDL_Thread *thread;
+    // Condition variable to force a draw from controller, and to wait on to 
+    // prevent continuous draw calls that are resource intensive.
+    SDL_cond *draw_condition;
+    // The current view area of the model.
+    ViewPort *port;
+    // SDL timer that calls a callback to notify draw_condition, to draw to the 
+    // window at a desired rate (60Hz).
+    SDL_TimerID framerate_control_timer;
+    // Timer that calls a callback to print how many frames have been rednered
+    // in the last second, and reset the fps counter.
+    SDL_TimerID fps_counter_timer;
+    // Frames per second, locked under View->mutex.
+    int fps;
+    // Mutex to stop the controller and view from accessing done concurrently.
+    SDL_mutex *done_mutex;
+    // Flag to indicate that the view thread should terminate.
+    bool done;
+    // Function that can be binded to draw onto the window. Takes this view to
+    // draw onto and a void* as the drawn data.
+    void(*draw_function)(View*, void*);
+    // Data to pass to draw_function.
+    void *data;
+};
 
 /**
  * Create a view of the game, that periodically accesses the model and uses the 
  * retrieved data to draw to the window.
  * 
  * @param window Pointer to the window to draw onto.
- * @param spin Spin model to draw.
+ * @param port The initial view port to use.
+ * @param draw_function Function to call to draw onto the view. Takes a view
+ * object to draw onto, and a void * to pass the drawn data.
+ * @param data The data to pass to draw_function.
  * 
  * @return A pointer to the view, to use with the rest of the view interface.
  */
-View *view_create(SDL_Window *window, Spin *spin);
+View *view_create(
+    SDL_Window *window,
+    ViewPort *port,
+    void(*draw_function)(View*, void*),
+    void *data
+);
 
 /**
  * Thread function that runs to clear, draw and render to the view window. 
@@ -50,6 +91,18 @@ void view_notify(View *view);
 bool view_done(View *view);
 
 /**
+ * Callback to pass to an internal SDL timer to notify the view to draw every
+ * given number of milliseconds.
+ */
+uint32_t view_notify_callback(uint32_t interval, void *data);
+
+/**
+ * Callback to pass to an internal SDL timer (that calls this each second) to
+ * calculate the number of frames per second since the last call.
+ */
+uint32_t view_fps_counter_callback(uint32_t interval, void *data);
+
+/**
  * Resize the window controlled by the view.
  * 
  * The new width and height must be greater than zero.
@@ -75,14 +128,6 @@ void view_move(View *view, Direction direction, bool state);
  * @param view The view to which to draw the grid.
  */
 void view_draw_grid(View *view);
-
-/**
- * Draw a spin object to a renderer.
- * 
- * @param renderer The renderer to draw the spin model with.
- * @param spin The spin object to draw.
- */
-void spin_view_update(View *view, SpinData *spin);
 
 /**
  * Terminates the running View thread, destroys view contents and deallocates
