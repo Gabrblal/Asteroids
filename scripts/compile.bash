3c3145680fdca658d90f7cd599b4a1f16682d553
Gabriel Blomberg <gabrielblomberg0@gmail.com>
2021-07-22 20:30:31 +1000

    Fixed compile.bash directory and -lm.

--- scripts/compile.bash
+++ scripts/compile.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+cd ..
+
 gcc -g -o bin/Asteroids \
     -Isrc $(find src -name '*.c') \
-    $(sdl2-config --cflags --libs) \
+    $(sdl2-config --cflags --libs) -static-libgcc -lm\
     -Wall -Werror -Wpedantic
 
 exit $?
